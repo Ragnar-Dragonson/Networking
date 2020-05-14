f214ef6a137b2be4debd439c99ad9ec01ba12b95
Ragnar Clarkson <49516716+Ragnar-Dragonson@users.noreply.github.com>
2020-05-14 01:18:03 +0100

    Add files via upload

---
+++ Networking/location/location/ClientUserInterface.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace location
+{
+    partial class ClientUserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.updateRadiobtn = new System.Windows.Forms.RadioButton();
+            this.locateRadiobtn = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.locationTextbox = new System.Windows.Forms.TextBox();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.onePointoneRadiobtn = new System.Windows.Forms.RadioButton();
+            this.oneRadiobtn = new System.Windows.Forms.RadioButton();
+            this.nineRadiobtn = new System.Windows.Forms.RadioButton();
+            this.whoisRadiobtn = new System.Windows.Forms.RadioButton();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 16);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Output will be returned here:";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Enabled = false;
+            this.outputTextBox.Location = new System.Drawing.Point(38, 271);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(198, 100);
+            this.outputTextBox.TabIndex = 54;
+            this.outputTextBox.Text = "";
+            // 
+            // updateRadiobtn
+            // 
+            this.updateRadiobtn.AutoSize = true;
+            this.updateRadiobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateRadiobtn.Location = new System.Drawing.Point(151, 199);
+            this.updateRadiobtn.Name = "updateRadiobtn";
+            this.updateRadiobtn.Size = new System.Drawing.Size(71, 20);
+            this.updateRadiobtn.TabIndex = 53;
+            this.updateRadiobtn.TabStop = true;
+            this.updateRadiobtn.Text = "Update";
+            this.updateRadiobtn.UseVisualStyleBackColor = true;
+            // 
+            // locateRadiobtn
+            // 
+            this.locateRadiobtn.AutoSize = true;
+            this.locateRadiobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locateRadiobtn.Location = new System.Drawing.Point(48, 198);
+            this.locateRadiobtn.Name = "locateRadiobtn";
+            this.locateRadiobtn.Size = new System.Drawing.Size(67, 20);
+            this.locateRadiobtn.TabIndex = 52;
+            this.locateRadiobtn.TabStop = true;
+            this.locateRadiobtn.Text = "Locate";
+            this.locateRadiobtn.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(333, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(256, 16);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Please select the protocol you wish to use";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 16);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Please select the type of request you wish to do";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(362, 16);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "Please enter the location you wish to update the person with:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 16);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "Please enter the name of the person you wish to find or update:";
+            // 
+            // locationTextbox
+            // 
+            this.locationTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationTextbox.Location = new System.Drawing.Point(413, 95);
+            this.locationTextbox.Name = "locationTextbox";
+            this.locationTextbox.Size = new System.Drawing.Size(100, 22);
+            this.locationTextbox.TabIndex = 47;
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextbox.Location = new System.Drawing.Point(413, 35);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(100, 22);
+            this.nameTextbox.TabIndex = 46;
+            // 
+            // onePointoneRadiobtn
+            // 
+            this.onePointoneRadiobtn.AutoSize = true;
+            this.onePointoneRadiobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onePointoneRadiobtn.Location = new System.Drawing.Point(343, 282);
+            this.onePointoneRadiobtn.Name = "onePointoneRadiobtn";
+            this.onePointoneRadiobtn.Size = new System.Drawing.Size(80, 20);
+            this.onePointoneRadiobtn.TabIndex = 45;
+            this.onePointoneRadiobtn.TabStop = true;
+            this.onePointoneRadiobtn.Text = "HTTP1.1";
+            this.onePointoneRadiobtn.UseVisualStyleBackColor = true;
+            // 
+            // oneRadiobtn
+            // 
+            this.oneRadiobtn.AutoSize = true;
+            this.oneRadiobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oneRadiobtn.Location = new System.Drawing.Point(343, 259);
+            this.oneRadiobtn.Name = "oneRadiobtn";
+            this.oneRadiobtn.Size = new System.Drawing.Size(80, 20);
+            this.oneRadiobtn.TabIndex = 44;
+            this.oneRadiobtn.TabStop = true;
+            this.oneRadiobtn.Text = "HTTP1.0";
+            this.oneRadiobtn.UseVisualStyleBackColor = true;
+            // 
+            // nineRadiobtn
+            // 
+            this.nineRadiobtn.AutoSize = true;
+            this.nineRadiobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nineRadiobtn.Location = new System.Drawing.Point(343, 236);
+            this.nineRadiobtn.Name = "nineRadiobtn";
+            this.nineRadiobtn.Size = new System.Drawing.Size(80, 20);
+            this.nineRadiobtn.TabIndex = 43;
+            this.nineRadiobtn.TabStop = true;
+            this.nineRadiobtn.Text = "HTTP0.9";
+            this.nineRadiobtn.UseVisualStyleBackColor = true;
+            // 
+            // whoisRadiobtn
+            // 
+            this.whoisRadiobtn.AutoSize = true;
+            this.whoisRadiobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.whoisRadiobtn.Location = new System.Drawing.Point(343, 213);
+            this.whoisRadiobtn.Name = "whoisRadiobtn";
+            this.whoisRadiobtn.Size = new System.Drawing.Size(64, 20);
+            this.whoisRadiobtn.TabIndex = 42;
+            this.whoisRadiobtn.TabStop = true;
+            this.whoisRadiobtn.Text = "Whois";
+            this.whoisRadiobtn.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(443, 333);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(133, 57);
+            this.submitButton.TabIndex = 41;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // ClientUserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 416);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.updateRadiobtn);
+            this.Controls.Add(this.locateRadiobtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.locationTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.onePointoneRadiobtn);
+            this.Controls.Add(this.oneRadiobtn);
+            this.Controls.Add(this.nineRadiobtn);
+            this.Controls.Add(this.whoisRadiobtn);
+            this.Controls.Add(this.submitButton);
+            this.Name = "ClientUserInterface";
+            this.Text = "Client User Interface";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.RadioButton updateRadiobtn;
+        private System.Windows.Forms.RadioButton locateRadiobtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox locationTextbox;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.RadioButton onePointoneRadiobtn;
+        private System.Windows.Forms.RadioButton oneRadiobtn;
+        private System.Windows.Forms.RadioButton nineRadiobtn;
+        private System.Windows.Forms.RadioButton whoisRadiobtn;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
